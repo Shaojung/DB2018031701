bd65f4ef614790c2302a654761b929e4dabf82b4
Shaojung <shaojung@gmail.com>
2018-03-17 14:02:22 +0800

    location

--- app/src/main/java/com/example/student/db2018031701/MainActivity.java
+++ app/src/main/java/com/example/student/db2018031701/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             Log.d("LOC", "沒有權限");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+
             return;
         }
         Log.d("LOC", "有權限");
